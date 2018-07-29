0bd70b8fbefda5db8fe423644c866c0f51955476
ashish1997it <sondagarashish@gmail.com>
2018-07-30 00:30:10 +0530

    chnages

--- model/config/ProjPath.php
+++
@@ -1,6 +0,0 @@
-<?php
-$projPath ='/proj18/php/0Demo/CodeMagic';	//home
-//$projPath ='/myTest/php/CodeMagic';	//office
-//$projPath ='http://rawdd.co.nf/proj18/CodeMagic';	// staging server
-
-?>
--- view/PageLogOut.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//hide
-session_start();
-session_destroy(); // Is Used To Destroy All Sessions
-
-header("Refresh:2; url=../view/indexNew.php");
-exit('destroy all session.. ');
-?>
